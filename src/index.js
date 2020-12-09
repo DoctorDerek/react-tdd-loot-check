8a5638c9ab84fcccf806f9a44354938fe3c80c4e
Dr. Derek Austin <53202242+djD-REK@users.noreply.github.com>
2020-12-08 19:11:16 -0500

    Tested action & reducer

--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
 import React from "react"
 import { render } from "react-dom"
+import { createStore } from "redux"
+import rootReducer from "./reducers/balance"
+import { Provider } from "react-redux"
 import App from "./components/App"
 import AppFunctionComponent from "./components/AppFunctionComponent"
 
-render(<App />, document.getElementById("root1"))
-render(<AppFunctionComponent />, document.getElementById("root2"))
+const store = createStore(rootReducer)
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root1")
+)
+render(
+  <Provider store={store}>
+    <AppFunctionComponent />
+  </Provider>,
+  document.getElementById("root2")
+)
